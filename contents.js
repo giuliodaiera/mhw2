40589eb14fee7f9906c39ad703b9fba8eada73c2
Giulio D'Aiera <81303732+giuliodaiera@users.noreply.github.com>
2021-04-12 23:20:01 +0200

    Add files via upload

---
+++ contents.js
@@ -0,0 +1,69 @@
+const giocatori = 
+[
+
+    {
+        nome: 'Cristiano Ronaldo',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/13029.jpg",
+        statistiche: 'Gol:25, Presenze:26, Rigori:5',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Romelu Lukaku',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/142964.jpg",
+        statistiche: 'Gol:21, Presenze:28, Rigori:5',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Luis Muriel',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/160625.jpg",
+        statistiche: 'Gol:18, Presenze:27, Rigori:1',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Zlatan Ibrahimovic',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/1274.jpg",
+        statistiche: 'Gol:15, Presenze:16, Rigori:3',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Andrea Belotti',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/208153.jpg",
+        statistiche: 'Gol:11, Presenze:26, Rigori:2',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Lorenzo Insigne',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/160316.jpg?fallback=png",
+        statistiche: 'Gol:15, Presenze:26, Rigori:6',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Ciro Immobile',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/137191.jpg",
+        statistiche: 'Gol:14, Presenze:26, Rigori:3',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Francesco Caputo',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/124744.jpg",
+        statistiche: 'Gol:11, Presenze:22, Rigori:3',
+        cuore: "heart.png",
+    },
+
+    {
+        nome: 'Roberto Soriano',
+        immagine: "https://s.hs-data.com/bilder/spieler/gross/134674.jpg",
+        statistiche: 'Gol:6, Presenze:26, Rigori:2',
+        cuore: "heart.png",
+    },
+
+]
+
+
